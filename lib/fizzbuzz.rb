290edd3d80de00b72c59f13b74d3fbf49e614704
Colin McCulloch <mccolane@gmail.com>
2015-11-18 19:48:10 +0000

    Refactored

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,24 +1,6 @@
 def fizzbuzz(number)
-
-if number % 3 == 0 && number % 5 == 0
-  'fizzbuzz'
-
-
-elsif number % 3 == 0
-    'fizz'
-
-elsif number % 5 == 0
-    'buzz'
-
-else
+  return 'fizzbuzz' if number % 3 == 0 && number % 5 == 0
+  return 'fizz' if number % 3 == 0
+  return 'buzz' if number % 5 == 0
   number
-
-
-
-
-
-  end
-
-
-
 end
